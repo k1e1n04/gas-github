e02d2ef06e0b3e23184bb0737ec38551dd02dfd0
KenIshii <riguveda44@gmail.com>
2024-05-06 15:50:21 +0900

    fix PullRequestSummaryRepository

--- src/repositories/PullRequestSummaryRepository.ts
+++ src/repositories/PullRequestSummaryRepository.ts
@@ -1,37 +1,25 @@
 import { PullRequestSummary } from "@/models/pulls/PullRequestSummary";
-import {utils} from "@/utils/utils";
+import { utils } from "@/utils/utils";
 
 /**
  * This class is responsible for handling the data access of the PullRequestSummary entity.
  */
 export class PullRequestSummaryRepository {
-  private readonly sheet: GoogleAppsScript.Spreadsheet.Sheet;
-
-  /**
-   * Constructor
-   * @param summaries
-   * @param sheet
-   */
-    constructor(
-        summaries: PullRequestSummary[],
-        sheet: GoogleAppsScript.Spreadsheet.Sheet
-  ) {
-        this.sheet = sheet;
-    }
+  private readonly sheetName: string = "PullRequestSummary";
 
   /**
    * Store the PullRequestSummary data to the Google Spreadsheet
    * @param summaries
    */
   store(summaries: PullRequestSummary[]): void {
-    utils.appendDataToSpreadsheet(this.sheet, this.toRecords(summaries));
+    utils.appendDataToSpreadsheet(this.sheetName, this.toRecords(summaries));
   }
 
-    /**
-     * Convert PullRequestSummary data to the Google Spreadsheet record format
-     * @param summaries
-     * @returns
-     */
+  /**
+   * Convert PullRequestSummary data to the Google Spreadsheet record format
+   * @param summaries
+   * @returns
+   */
   private toRecords(summaries: PullRequestSummary[]) {
     return summaries.map((summary) => {
       return summary.toRecord();
